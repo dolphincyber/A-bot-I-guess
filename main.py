f0695815196d31ff1964c9ae9c9203097c8607f8
dolphincyber <57072181+dolphincyber@users.noreply.github.com>
2020-09-15 14:17:37 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import discord
+from discord.ext import commands
+import asyncio
+import random
+
+client = commands.Bot(command_prefix = '.')
+client.remove_command("help")
+
+client.run('NzU1NTMzMjMyNDE3ODAwMTky.X2ErJw.3dYeQHGjdrbiNaIkixtbHbdVdHM')
