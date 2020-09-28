97c9c7acd5c169ea7898f8648ea8a3557c8f0dde
dolphincyber <ethan.zhao.us@gmail.com>
2020-09-28 12:30:07 -0700

    changed name to 'NEBULA'

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ async def on_ready():
 @client.command(pass_context=True)
 async def help(ctx, page=0):
     if page == 0:
-        embed = discord.Embed(title="Cury Bot Help Page", description="Go to the page you need by typing the command with the page number after it. (e.g. .help 1)", color=3447003)
+        embed = discord.Embed(title="Nebula Help Page", description="Go to the page you need by typing the command with the page number after it. (e.g. .help 1)", color=3447003)
         embed.add_field(name="`1` Server Utilities.", value="****", inline=False)
         embed.add_field(name="`2` Fun.", value="****", inline=False)
         embed.set_footer(text="home page")
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/626633106803392513/755532296903196852/Curry.jpg')
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/643685845953675307/760220420916903996/Untitled.jpg')
         await ctx.send(embed=embed)
     if page == 1:
         embed = discord.Embed(title="Server Utilities", description="some server utils", inline=False)
@@ -34,7 +34,7 @@ async def help(ctx, page=0):
         # embed.add_field(name="mute", value="shut someone up. thats it.", inline=False)
         # embed.add_field(name="unmute", value="UN-shut someone up. idek why you would use this smh", inline=False)
         embed.set_footer(text=f"page 1 of 3")
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/626633106803392513/755532296903196852/Curry.jpg')
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/643685845953675307/760220420916903996/Untitled.jpg')
         await ctx.send(embed=embed)
     if page == 2:
         embed = discord.Embed(title="Fun", description="", inline=False)
@@ -42,12 +42,19 @@ async def help(ctx, page=0):
         embed.add_field(name="google", value="search google for something", inline = False)
         embed.add_field(name="spotify", value="look for a song on spotify", inline = False)
         embed.add_field(name="eightball", value="ask the so called 'mAgIC' eight ball a question", inline = False)
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/626633106803392513/755532296903196852/Curry.jpg')
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/643685845953675307/760220420916903996/Untitled.jpg')
         embed.set_footer(text=f"page 2 of 3")
         await ctx.send(embed=embed)
     if page == 3:
         embed = discord.Embed(title="currency", description="", inline=False)
-
+        embed.add_field(name="start", value="Register for the currency feature", inline=False)
+        embed.add_field(name="bal", value="Check how many coins you have", inline=False)
+        embed.add_field(name="collect", value="go collect some coins :)", inline=False)
+        embed.add_field(name="beg", value="beg some people for coins", inline=False)
+        embed.add_field(name="daily", value="get your daily boost of coins", inline=False)
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/643685845953675307/760220420916903996/Untitled.jpg')
+        embed.set_footer(text=f"page 3 of 3")
+        
 
 #info
 @client.command()
@@ -63,18 +70,29 @@ async def info(ctx):
 @client.command()
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount):
+    amount=int(amount)
     await ctx.channel.purge(limit = amount + 1)
 
+# bug
 @client.command()
-async def botannounce(ctx, *, code):
-    verfied="jKI8ncOKTQ5dBf"
-    if code == verfied:
-        await ctx.send("check your cmd line")
-        announcement=input("what do you want to announce?\n")
-        await ctx.channel.purge(limit = 2 + 1)
-        await ctx.send(announcement)
-    if code != verfied:
-        await ctx.send("you aren't the bot creator or an admin!!!")
+@commands.cooldown(1, 300, commands.BucketType.user)
+async def bug(ctx, *, report):
+    embed = discord.Embed(title="bug report", description="filed by " + str(ctx.message.author))
+    embed.add_field(name=report, value="🛑🛑🛑", inline=True)
+    user = client.get_user(600076125556965376)
+    await user.send(embed=embed)
+    await ctx.send("Thanks for reporting this bug!")
+@bug.error
+async def bug_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        msg = '{:.2f}s'.format(error.retry_after)
+        embed=discord.Embed(
+            colour = discord.Colour.red()
+        )
+        embed.add_field(name='You already ran this command! Please try again in ', value=msg)
+        await ctx.send(embed=embed)
+    else:
+        raise error
 
 # # Kick
 # @client.command()
@@ -161,11 +179,15 @@ async def spotify(ctx, *, search):
     await ctx.send('https://open.spotify.com/search/' + refinesearch)
 
 # Watch
-@client.command()   
+@client.command()
 async def watch(ctx, *, search):
-     results = YoutubeSearch(search, max_results=1).to_dict()
-     x = results[0]
-     await ctx.send('https://www.youtube.com' + x['url_suffix'])
+    try:
+        results = YoutubeSearch(search, max_results=1).to_dict()
+        x = results[0]
+        await ctx.send('https://www.youtube.com' + x['url_suffix'])
+    except IndexError:
+        embed = discord.Embed(title='🚫 No results found for', description=f'"{search}"', color=15158332)
+        await ctx.send(embed=embed)
 
 # 8ball
 @client.command()
@@ -213,13 +235,53 @@ def _save():
 #start
 @client.command(pass_context=True)
 async def start(ctx):
-   id = str(ctx.message.author.id)
-   if id not in amounts.keys():
+    id = str(ctx.message.author.id)
+    if id not in amounts.keys():
        amounts[id]=0
        await ctx.send("You are now registered")
        _save()
-   else:
+    else:
        await ctx.send("You already have an account")
+
+# shop
+@client.command(pass_context=True)
+@commands.cooldown(1, 3, commands.BucketType.user)
+async def shop(ctx, *,page=0):
+    id = str(ctx.message.author.id)
+    if id in amounts:
+        if page == 0:
+            embed = discord.Embed(title='Shop', description="buy some EPIC stuff here", colour = discord.Colour.blurple())
+            embed.add_field(name='Pages', value="**** - ____ - \n**** - ____ - \n**** - ____ - \n**** - ____ -", inline=False)
+            embed.set_footer(text="Page 0 of 3\ndo shop page_number to go to the page you want")
+            await ctx.send(embed=embed)
+        if page == 1:
+            embed = discord.Embed(title='Shop', description="buy some EPIC stuff here", colour = discord.Colour.blurple())
+            embed.add_field(name='Tools', value="idek yet...", inline=False)
+            embed.set_footer(text="Page 1 of 3\ndo shop page_number to go to the page you want")
+            await ctx.send(embed=embed)
+        if page == 2:
+            embed = discord.Embed(title='Shop', description="buy some EPIC stuff here", colour = discord.Colour.blurple())
+            embed.add_field(name='Collectables', value="🍬 **Breath Mint** - __5__ - i guess if your breath smells...\n\n🧀 **cheese slices** - __10__ - because why not?\n\n🍪 **cookies** - __15__ - just a collectable\n\n🎻 **violin** - __2,000__ because everyone knows that orchesthra > band\n\n🌌 **galaxy badge** - __100,000__ - badge for very comited members!\n\n🎖️ **galaxy medal** - __1,000,000__ - an extremely rare medal for the richest people\n\n🏆 **galaxy trophy** - __10,000,000__ - extremely rare trophy for the richest people\n\n", inline=False)
+            embed.set_footer(text="Page 2 of 3\ndo shop page_number to go to the page you want")
+            await ctx.send(embed=embed)
+        if page == 3:
+            embed = discord.Embed(title='Shop', description="buy some EPIC stuff here", colour = discord.Colour.blurple())
+            embed.add_field(name='Special', value="nothing yet...", inline=False)
+            embed.set_footer(text="Page 3 of 3\ndo shop page_number to go to the page you want")
+            await ctx.send(embed=embed)
+    else:
+        await ctx.send("You do not have an account")
+@shop.error
+async def shop_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        msg = '{:.2f}s'.format(error.retry_after)
+        embed=discord.Embed(
+            colour = discord.Colour.red()
+        )
+        embed.add_field(name='You already ran this command! Please try again in ', value=msg)
+        await ctx.send(embed=embed)
+    else:
+        raise error
 
 #bal
 @client.command(pass_context=True)
@@ -227,17 +289,25 @@ async def start(ctx):
 async def bal(ctx):
     id = str(ctx.message.author.id)
     if id in amounts:
-        embed = discord.Embed(
-           colour = discord.Colour.green()
-        )
-        embed.add_field(name='Your balance:', value=amounts[id])
-        embed.set_footer(text="look at all that curry")
-        await ctx.send(embed=embed)
+        if amounts[id] == 69420:
+            embed = discord.Embed(
+                colour = discord.Colour.green()
+            )
+            embed.add_field(name='Your balance:', value=amounts[id])
+            embed.set_footer(text="LMAO WAS THAT ON PURPOSE??")
+            await ctx.send(embed=embed)
+            return
+        else:
+            embed = discord.Embed(
+               colour = discord.Colour.green()
+            )
+            embed.add_field(name='Your balance:', value=amounts[id])
+            embed.set_footer(text="look at all those beans")
+            await ctx.send(embed=embed)
     else:
         await ctx.send("You do not have an account")
-    await asyncio.sleep(15)
 @bal.error
-async def cmd_error(ctx, error):
+async def bal_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
         msg = '{:.2f}s'.format(error.retry_after)
         embed=discord.Embed(
@@ -250,19 +320,37 @@ async def cmd_error(ctx, error):
 
 # cook
 @client.command()
-@commands.cooldown(1, 30, commands.BucketType.user)
-async def cook(ctx):
+@commands.cooldown(1, 0, commands.BucketType.user)
+async def collect(ctx):
     amount=random.randint(0, 300)
     amount=int(amount)
-    await ctx.send("you cooked some curry for your friends and they gave you " + str(amount) + " coins.")
+    chance=random.randint(0,100)
+    if chance > 70 and chance < 80:
+        await ctx.send("You collected " + str(amount) + " coins, AND a 🍬 **Breath Mint**!!!")
+        return
+    if chance > 80 and chance < 90:
+        await ctx.send("You collected " + str(amount) + " coins, AND a 🧀 **cheese slices**!!!")
+        return
+    if chance > 90 and chance < 95:
+        await ctx.send("You collected " + str(amount) + " coins, AND a 🎻 **violin**!!!")
+        return
+    if chance > 95 and chance < 95:
+        await ctx.send("You collected " + str(amount*2) + " coins. If you didn't know, you got DOUBLE THE COINS!")
+        return
+    if chance > 99:
+        await ctx.send("You collected " + str(amount) + " coins, AND a 🌌 **galaxy badge**!!!!!!!!! LUCKY!!!")
+        return
+    else:
+        await ctx.send("You collected " + str(amount) + " coins.")
+        return
     id = str(ctx.message.author.id)
     if id in amounts:
         amounts[id]
         amounts[id] += amount
         with open('amounts.json', 'w+') as f:
             json.dump(amounts, f)
-@cook.error
-async def cook_error(ctx, error):
+@collect.error
+async def collect_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
         msg = '{:.2f}s'.format(error.retry_after)
         embed=discord.Embed(
@@ -306,7 +394,7 @@ async def daily_error(ctx, error):
 @commands.cooldown(1, 45, commands.BucketType.user)
 async def beg(ctx):
     id = str(ctx.message.author.id)
-    await ctx.send("Where do you want to beg at?\n`S` Street\n`K` Soup kitchen\n`R`That random guys house\n`A` In the middle of an airport")
+    await ctx.send("Where do you want to beg at?\n`S` Street\n`H` Hospital\n`R`That random guys house\n`A` In the middle of an airport")
     ans = await ctx.bot.wait_for('message', check=lambda message: message.author == ctx.author)
     if ans.content.lower() == "s":
         poss=random.randint(0,100)
@@ -319,16 +407,34 @@ async def beg(ctx):
                     json.dump(amounts, f)
                 return
         num=random.randint(0, 329)
-        await ctx.send("So you didn't get run over by a car, and managed to get " + str(num) + " coins from people who liked the smell of your curry.")
+        chance=random.randint(0,100)
+        if chance > 70 and chance < 80:
+            await ctx.send("So you didn't get run over by a car, and managed to get " + str(num) + " coins, AND a 🍬 **Breath Mint**!!!")
+            return
+        if chance > 80 and chance < 90:
+            await ctx.send("So you didn't get run over by a car, and managed to get " + str(num) + " coins, AND a 🧀 **cheese slices**!!!")
+            return
+        if chance > 90 and chance < 95:
+            await ctx.send("So you didn't get run over by a car, and managed to get " + str(num) + " coins, AND a 🎻 **violin**!!!")
+            return
+        if chance > 95 and chance < 95:
+            await ctx.send("So you didn't get run over by a car, and managed to get " + str(num*2) + " coins. If you didn't know, you got DOUBLE THE COINS!")
+            return
+        if chance > 99:
+            await ctx.send("So you didn't get run over by a car, and managed to get " + str(num) + " coins, AND a 🌌 **galaxy badge**!!!!!!!!! LUCKY!!!")
+            return
+        else:
+            await ctx.send("So you didn't get run over by a car, and managed to get " + str(num) + " coins from people who liked your stories.")
+            return
         if id in amounts:
             amounts[id]
             amounts[id] += num
             with open('amounts.json', 'w+') as f:
                 json.dump(amounts, f)
-    if ans.content.lower() == "k":
+    if ans.content.lower() == "h":
         poss=random.randint(0,100)
         if poss > 75:
-            await ctx.send('Bro what were you thinking? This is a SOUP kitchen not a "beging for people to pay me for my curry" kitchen.')
+            await ctx.send('Famous last words: "i wont get covid here lol".')
             if id in amounts:
                 amounts[id]
                 amounts[id]=0
@@ -336,7 +442,25 @@ async def beg(ctx):
                     json.dump(amounts, f)
                 return
         num=random.randint(0,250)
-        await ctx.send("The people at the kitchen like curry better than soup, and the manager payed you " + str(num) + " coins for your secret recipe.")
+        chance=random.randint(0,100)
+        if chance > 70 and chance < 80:
+            await ctx.send("You didnt get covid... and got " + str(num) + " coins, AND a 🍬 **Breath Mint**!!!")
+            return
+        if chance > 80 and chance < 90:
+            await ctx.send("You didnt get covid... and got " + str(num) + " coins, AND a 🧀 **cheese slices**!!!")
+            return
+        if chance > 90 and chance < 95:
+            await ctx.send("You didnt get covid... and got " + str(num) + " coins, AND a 🎻 **violin**!!!")
+            return
+        if chance > 95 and chance < 95:
+            await ctx.send("You didnt get covid... and got " + str(num*2) + " coins. If you didn't know, you got DOUBLE THE COINS!")
+            return
+        if chance > 99:
+            await ctx.send("You didnt get covid... and got " + str(num) + " coins, AND a 🌌 **galaxy badge**!!!!!!!!! LUCKY!!!")
+            return
+        else:
+            await ctx.send("You didnt get covid... and got " + str(num) + " coins as a prize!")
+            return
         if id in amounts:
             amounts[id]
             amounts[id] += num
@@ -361,7 +485,25 @@ async def beg(ctx):
                     json.dump(amounts, f)
                 return
         num=random.randint(0,999)
-        await ctx.send("lucky you! you manged to steal " + str(num) + " coins from under the pilot's noses.")
+        chance=random.randint(0,100)
+        if chance > 70 and chance < 80:
+            await ctx.send("Noice. you manged to steal " + str(num) + " coins, AND a 🍬 **Breath Mint**!!!")
+            return
+        if chance > 80 and chance < 90:
+            await ctx.send("Noice. you manged to steal " + str(num) + " coins, AND a 🧀 **cheese slices**!!!")
+            return
+        if chance > 90 and chance < 95:
+            await ctx.send("Noice. you manged to steal " + str(num) + " coins, AND a 🎻 **violin**!!!")
+            return
+        if chance > 95 and chance < 95:
+            await ctx.send("Noice. you manged to steal " + str(num*2) + " coins. If you didn't know, you got DOUBLE THE COINS!")
+            return
+        if chance > 99:
+            await ctx.send("Noice. you manged to steal " + str(num) + " coins, AND a 🌌 **galaxy badge**!!!!!!!!! LUCKY!!!")
+            return
+        else:
+            await ctx.send("Noice. you manged to steal " + str(num) + "  coins from under the pilot's noses.")
+            return
         if id in amounts:
             amounts[id]
             amounts[id] += num
@@ -380,4 +522,4 @@ async def beg_error(ctx, error):
         raise error
 
 
-client.run('')
+client.run('NzU1NTMzMjMyNDE3ODAwMTky.X2ErJw.X4R2oTUi84qj-5lsIGbPS76uwkk')
